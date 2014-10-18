6009bf35795745a3ddd71edeada8cdf44e01de3d
Star Brilliant <m13253@hotmail.com>
2014-10-18 20:31:25 +0800

    Update

--- src/weproclient.js
+++ src/weproclient.js
@@ -5,9 +5,15 @@
 (function(window) {
 "use strict";
 
-var OpenWeproError = function(what) {
+function OpenWeproError(what) {
     return Error("[OpenWepro] " + what);
 };
+function TraceBackError(e) {
+    if(e.message && e.stack)
+        console.error(e.message + "\n\n" + e.stack);
+    else
+        console.error(e);
+}
 
 var urlPrefix = "@path_prefix@";
 var urlMatcher = new RegExp("^/(.*?)/(.*?)/:(?:/(.*))?$");
@@ -36,7 +42,7 @@ function convertCSS(text) {
     var convMatcher = new RegExp("^([\\S\\s]*?[\\s:,])url\\s*\\(\\s*([\"']?)(.*?)\\2\\s*\\)([\\S\\s]*)$", "i");
     var converted = ["/* OpenWepro */\n"];
     var matched;
-    while(matched = convMatcher.exec(text)) {
+    while((matched = convMatcher.exec(text))) {
         converted.push(matched[1] + "url(" + matched[2] + convertURL(matched[3]) + matched[2] + ")");
         text = matched[4];
     }
@@ -45,7 +51,7 @@ function convertCSS(text) {
 }
 
 var oldXMLHttpRequestOpen = XMLHttpRequest.prototype.open;
-XMLHttpRequest.prototype.open = function(method, url, async) {
+XMLHttpRequest.prototype.open = function open(method, url, async) {
     return oldXMLHttpRequestOpen.call(this, method, convertURL(url), async !== false);
 };
 
@@ -58,7 +64,7 @@ var documentWritePoint = null; /* document.write */
 Object.defineProperty(Element.prototype, "_OpenWeproAttributes", {
     configurable: false,
     enumerable: false,
-    get: function() {
+    get: function get() {
         if(!this.__OpenWeproAttributes)
             Object.defineProperty(this, "__OpenWeproAttributes", {
                 configurable: false,
@@ -67,17 +73,17 @@ Object.defineProperty(Element.prototype, "_OpenWeproAttributes", {
             });
         return this.__OpenWeproAttributes;
     },
-    set: function(value) {
+    set: function set(value) {
         throw OpenweproError("can not write read-only property _OpenWeproAttributes");
     }
 });
-Element.prototype.getAttribute = function(attr) {
+Element.prototype.getAttribute = function getAttribute(attr) {
     return this._OpenWeproAttributes["attr_" + attr] || oldGetAttribute.call(this, attr);
 };
-Element.prototype.hasAttribute = function(attr) {
+Element.prototype.hasAttribute = function hasAttribute(attr) {
     return "attr_" + attr in this._OpenWeproAttributes || oldHasAttribute.call(this, attr);
 };
-Element.prototype.setAttribute = function(attr, value) {
+Element.prototype.setAttribute = function setAttribute(attr, value) {
     /* console.log(this.nodeName.toLowerCase() + "." + attr + " = " + value); */
     if(attr === "crossorigin") {
         if(value === "anonymous") {
@@ -95,23 +101,20 @@ Element.prototype.setAttribute = function(attr, value) {
             var el = this;
             var xhr = new XMLHttpRequest();
             xhr.open("GET", value, this.hasAttribute("async"));
-            xhr.addEventListener("load", function() {
+            xhr.addEventListener("load", function onload() {
                 var oldDocumentWritePoint = documentWritePoint;
                 documentWritePoint = el;
                 if(!el.hasAttribute("defer") && xhr.status === 200)
                     try {
                         window.eval("/* " + value.replace('*/', '%2a/') + " */\n" + xhr.responseText);
                     } catch(e) {
-                        if(e.message && e.stack)
-                            console.error(e.message + "\n\n" + e.stack);
-                        else
-                            console.error(e);
+                        TraceBackError(e);
                     }
                 else
                     oldSetAttribute.call(el, attr, convertURL(value));
                 documentWritePoint = oldDocumentWritePoint;
             });
-            xhr.addEventListener("error", function() {
+            xhr.addEventListener("error", function onerror() {
                 var oldDocumentWritePoint = documentWritePoint;
                 documentWritePoint = el;
                 oldSetAttribute.call(el, attr, convertURL(value));
@@ -127,53 +130,110 @@ Element.prototype.setAttribute = function(attr, value) {
         return oldSetAttribute.call(this, attr, value);
     return value;
 };
-Element.prototype.removeAttribute = function(attr) {
+Element.prototype.removeAttribute = function removeAttribute(attr) {
     delete this._OpenWeproAttributes["attr_" + attr];
     oldRemoveAttribute.call(this, attr);
 };
+attrToInject.forEach(function attrToInjectEach(attr) {
+    if(attr !== "style")
+        try {
+            Object.defineProperty(Element.prototype, attr, {
+                configurable: true,
+                enumerable: true,
+                get: function get() { return this.hasAttribute(attr) ? this.getAttribute(attr) : undefined; },
+                set: function set(value) { this.setAttribute(attr, value); return value; }
+            });
+        } catch(e) {
+            TraceBackError(e);
+            console.warn("[OpenWepro] can not inject Element.prototype." + attr);
+        }
+});
 
 function updateElementAttribute(el, attr) {
-    if(attr !== "value" || (el.nodeName === "PARAM" && el.name === "movie")) {
+    if(attr !== "value" || (el.nodeName === "PARAM" && el.name === "movie"))
         if(el._OpenWeproAttributes && !("attr_" + attr in el._OpenWeproAttributes) && el.hasAttribute && el.hasAttribute(attr))
             el.setAttribute(attr, el.getAttribute(attr));
-        if(attr !== "style")
-            try {
-                Object.defineProperty(el, attr, {
-                    configurable: true,
-                    enumerable: true,
-                    get: function() { return this.hasAttribute(attr) ? this.getAttribute(attr) : undefined; },
-                    set: function(value) { this.setAttribute(attr, value); return value; }
-                });
-            } catch(e) {
-                if(e.message && e.stack)
-                    console.error(e.message + "\n\n" + e.stack);
-                else
-                    console.error(e);
-                console.warn("[OpenWepro] can not inject element." + attr);
-            }
-    }
-    return el;
-}
-
-function updateElementAttributes(el) {
-    if(el._OpenWeproAttributes)
-        attrToInject.forEach(function(attr) {
-            updateElementAttribute(el, attr);
-        });
     return el;
 }
 
 function injectNode(el) {
-    updateElementAttributes(el);
+    if(el._OpenWeproAttributes)
+        attrToInject.forEach(function injectNodeEach(attr) {
+            updateElementAttribute(el, attr);
+        });
     if(el.nodeName === "STYLE" && el.textContent.substr(0, 15) !== "/* OpenWepro */")
         el.textContent = convertCSS(el.textContent);
     return el;
 }
 
+var oldCreateElement = Document.prototype.createElement;
+Document.prototype.createElement = function createElement(tagName) {
+    return injectNode(oldCreateElement.call(this, tagName));
+};
+
+document.write = function write(markup) {
+    if(documentWritePoint) {
+        var documentWriteParent = documentWritePoint.parentNode;
+        var documentWriteReference = documentWritePoint.nextSibling;
+    } else {
+        var documentWriteParent = document.body || document.head || document.documentElement;
+        var documentWriteReference = null;
+    }
+    var tmp = document.createElement('body');
+    tmp.innerHTML = markup;
+    var frag = document.createDocumentFragment();
+    var child;
+    while((child = tmp.firstChild))
+        frag.appendChild(child);
+    documentWriteParent.insertBefore(frag, documentWriteReference);
+};
+document.writeln = function writeln(markup) {
+    return document.write(markup + "\n");
+};
+
+var oldCookie = document.cookie;
+try {
+    Object.defineProperty(document, "cookie", {
+        configurable: false,
+        get: function get() { return oldCookie; },
+        set: function set(value) {
+            console.warn("[OpenWepro] document.cookie has not been unimplemented yet: " + value);
+            return value;
+        }
+    });
+} catch(e) {
+    TraceBackError(e);
+    console.warn("[OpenWepro] can not inject document.cookie");
+}
+var oldDomain = document.domain;
+try {
+    Object.defineProperty(document, "domain", {
+        configurable: false,
+        get: function get() { return oldDomain; },
+        set: function set(value) {
+            console.warn("[OpenWepro] document.domain has not been unimplemented yet: " + value);
+            return value;
+        }
+    });
+} catch(e) {
+    TraceBackError(e);
+    console.warn("[OpenWepro] can not inject document.domain");
+}
+
+var oldWorker = window.Worker;
+if(oldWorker) {
+    window.Worker = function Worker(url) {
+        /* Thank you, http://stackoverflow.com/a/13839919/2557927 */
+        var unbind = Function.bind.bind(Function.bind);
+        return new (unbind(oldWorker, null).call(null, url));
+    };
+    window.Worker.__proto__ = oldWorker.__proto__;
+}
+
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 if(MutationObserver) {
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
+    var observer = new MutationObserver(function onMutation(mutations) {
+        mutations.forEach(function onMuataionEach(mutation) {
             if(mutation.type === "childList")
                 for(var i = 0; i < mutation.addedNodes.length; i++)
                     injectNode(mutation.addedNodes[i]);
@@ -191,78 +251,28 @@ if(MutationObserver) {
         subtree: true,
         attributeFilter: attrToInject
     };
-    observer.observe(document.documentElement, observerConfig);
-} else
-    console.warn("[OpenWepro] can not set up MutationObserver");
-
-var oldCreateElement = Document.prototype.createElement;
-Document.prototype.createElement = function(tagName) {
-    return injectNode(oldCreateElement.call(this, tagName));
-};
-
-var oldWorker = window.Worker;
-window.Worker = function(url) {
-    /* Thank you, http://stackoverflow.com/a/13839919/2557927 */
-    var unbind = Function.bind.bind(Function.bind);
-    return new (unbind(oldWorker, null).call(null, url));
-}
-window.Worker.__proto__ = oldWorker.__proto__;
-
-var oldCookie = document.cookie;
-try {
-    Object.defineProperty(document, "cookie", {
-        configurable: false,
-        get: function() {
-            return oldCookie;
-        }, set: function(value) {
-            console.warn("[OpenWepro] document.cookie has not been unimplemented yet: " + value);
-            return value;
-        }
-    });
-} catch(e) {
-    if(e.message && e.stack)
-        console.error(e.message + "\n\n" + e.stack);
-    else
-        console.error(e);
-    console.warn("[OpenWepro] can not inject document.cookie");
-}
-var oldDomain = document.domain;
-try {
-    Object.defineProperty(document, "domain", {
-        configurable: false,
-        get: function() {
-            return oldDomain;
-        }, set: function(value) {
-            console.warn("[OpenWepro] document.domain has not been unimplemented yet: " + value);
-            return value;
-        }
-    });
-} catch(e) {
-    if(e.message && e.stack)
-        console.error(e.message + "\n\n" + e.stack);
-    else
-        console.error(e);
-    console.warn("[OpenWepro] can not inject document.domain");
-}
-
-document.write = function(markup) {
-    if(documentWritePoint) {
-        var documentWriteParent = documentWritePoint.parentNode;
-        var documentWriteReference = documentWritePoint.nextSibling;
-    } else {
-        var documentWriteParent = document.body || document.head || document.documentElement;
-        var documentWriteReference = null;
+    try {
+        observer.observe(document.documentElement, observerConfig);
+    } catch(e) {
+        TraceBackError(e);
+        MutationObserver = null;
     }
-    var tmp = document.createElement('body');
-    tmp.innerHTML = markup;
-    var frag = document.createDocumentFragment();
-    var child;
-    while((child = tmp.firstChild))
-        frag.appendChild(child);
-    documentWriteParent.insertBefore(frag, documentWriteReference);
-};
-document.writeln = function(markup) {
-    return document.write(markup + "\n");
-};
+}
+if(!MutationObserver) {
+    console.warn("[OpenWepro] can not set up MutationObserver");
+    var thisScriptElement = document.getElementsByTagName("script");
+    thisScriptElement = thisScriptElement[thisScriptElement.length-1];
+    document.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
+        var elementIterator = function elementIterator(el) {
+            for(; el; el = el.nextElementSibling) {
+                if(el !== thisScriptElement)
+                    injectNode(el);
+                if(el.firstElementChild)
+                    elementIterator(el.firstElementChild);
+            }
+        };
+        elementIterator(document.firstElementChild);
+    });
+}
 
 }(this));
